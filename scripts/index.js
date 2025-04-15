1ae7b30c87bd687a4038b4b1409ceee65166ecbf
VishvamPatel31 <96796901+VishvamPatel31@users.noreply.github.com>
2025-04-14 22:23:29 -0400

    initial logic

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+console.clear()
+
+let input = document.getElementById("search-icon")
+
+let searchbtn = document.getElementById("searchbtn")
+
+searchbtn.addEventListener("")
+
+// fetch(`https://www.omdbapi.com/?apikey=5aa9157c&s=blade+runner&type=movie`)
+//     .then(res=>res.json())
+//     .then(data=>console.log(data))
